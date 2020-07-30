9dfd95793a2f11cb00026b46edca22c6f4a34e39
Andreea <andre.costea@gmail.com>
2020-07-31 00:34:49 +0800

    insert when both are unlocked

--- src/main/scala/TraverseJavaClass.scala
+++ src/main/scala/TraverseJavaClass.scala
@@ -21,7 +21,7 @@ object TraverseJavaClass  {
   def patchIDGenerator(len: Int): (Int, Int) = {
     val patchID_start = patchID_ref
     val patchID_stop  = patchID_ref + len + 1
-    patchID_ref = patchID_stop 1
+    patchID_ref = patchID_stop + 1
     (patchID_start, patchID_stop)
   }
 
@@ -82,6 +82,7 @@ object TraverseJavaClass  {
     }
   }
 
+  /* debugger */
   def generateInsertPatch(filename: String, insert: Insert, id: Int,
                           rewriter: TokenStreamRewriter,
                           tree: Java8Parser.CompilationUnitContext): Option[PatchResult] = {
@@ -98,31 +99,40 @@ object TraverseJavaClass  {
   /*                         FIX                      */
   /* ************************************************ */
   def applyPatch_def(filename: String,
-                 patch_id_str: String,
+                 patch_id: Int,
                  patches: List[(Int,Option[PatchResult])]): Unit = {
-    try {
-      val patch_id = patch_id_str.toInt
-      val patch = patches.foldLeft(None:Option[PatchResult])((acc:Option[PatchResult],
-                                                              ptc:(Int,Option[PatchResult])) => {
-        if (ptc._1 == patch_id) ptc._2
-        else acc
-      })
-      patch match {
-        case None => println("Invalid patch ID")
-        case Some(patch:PatchResult) =>
-          val rewriter  = patch.rewriter
-          rewriter.replace(patch.start,patch.stop,patch.patch)
+    val patch = patches.foldLeft(None: Option[PatchResult])((acc: Option[PatchResult],
+                                                             ptc: (Int, Option[PatchResult])) => {
+      if (ptc._1 == patch_id) ptc._2
+      else acc
+    })
+    patch match {
+      case None => println("Invalid patch ID")
+      case Some(patch: PatchResult) =>
+        val rewriter = patch.rewriter
+        rewriter.replace(patch.start, patch.stop, patch.patch)
 
-          /* write to file (keep the original one in `filename.orig` and the fix in `filename` */
-          val fm = new FileManipulation
-          fm.cloneOriginalFile(filename)
-          fm.overwriteOriginalFile(filename,rewriter.getText )
-      }
-    } catch {
-      case e: Exception => None
+        /* write to file (keep the original one in `filename.orig` and the fix in `filename` */
+        val fm = new FileManipulation
+        fm.cloneOriginalFile(filename)
+        fm.overwriteOriginalFile(filename, rewriter.getText)
     }
   }
 
+  def applyPatch_helper(filename: String,
+                     patch_id_str: String,
+                     patches: List[(Int,Option[PatchResult])]): Unit = {
+    try {
+      val patch_id = patch_id_str.toInt
+      applyPatch_def(filename,patch_id,patches)
+    } catch {
+      case _: Exception => {println("Invalid patch ID")
+        None
+      }
+    }
+  }
+
+  /* debugger */
   def applyPatch(filename: String,
                  patch_id_str: String,
                  patches: List[(Int,Option[PatchResult])]): Unit = {
@@ -130,7 +140,7 @@ object TraverseJavaClass  {
       println("Patch id: " + patch_id_str)
       println("Patches: "  + patches)
     }
-    applyPatch_def(filename,patch_id_str,patches)
+    applyPatch_helper(filename,patch_id_str,patches)
   }
 
   /* Generates a list of UPDATE objects meant to update the locks in
@@ -152,6 +162,17 @@ object TraverseJavaClass  {
       val insert = Insert("B",resource_summ.line,RacerDAPI.getResource2Var(resource_summ.resource),lck)
       insert::acc }
     )
+  }
+
+  /* Generates a list of INSERT objects for the resource in `resource_summ`
+     based on the locks available in summary `locks_summ` */
+  def generateInsertObjectOnCommonResource(summ1: CSumm, summ2: CSumm): List[Insert] = {
+    /* TODO  need to check which is that common resouce, e.g. myA or myA.f?
+    *   should be the outer most one, e.g. myA*/
+    val lck    = RacerDAPI.getResource2Var(summ1.resource)
+    val insert1 = Insert("B",summ1.line,RacerDAPI.getResource2Var(summ1.resource),lck)
+    val insert2 = Insert("B",summ2.line,RacerDAPI.getResource2Var(summ2.resource),lck)
+    List(insert1,insert2)
   }
 
   /* Generates a list of patches corresponding to the list of UPDATE objects (updates) */
@@ -198,8 +219,12 @@ object TraverseJavaClass  {
     /* currently they are manually crafted as below */
     /* {elem= Access: Read of this->myA Thread: AnyThread Lock: true Acquisitions: { P<0>{(this:B*)->myA2} } Pre: OwnedIf{ 0 }; loc= line 30; trace= { }},*/
     /* {elem= Access: Write to this->myA Thread: AnyThread Lock: true Acquisitions: { P<0>{(this:B*)->myA1} } Pre: OwnedIf{ 0 }; loc= line 24; trace= { }} },*/
-    val csumm1 = new CSumm("this->myA->f", Read, List("P<0>{(this:B*).myA2}"), 30 )
-    val csumm2 = new CSumm("this->myA", Write, List("P<0>{(this:B*).myA1}"), 24 )
+//    val csumm1 = new CSumm("this->myA->f", Read, List("P<0>{(this:B*).myA2}"), 30 )
+//    val csumm2 = new CSumm("this->myA", Write, List("P<0>{(this:B*).myA1}"), 24 )
+
+    val csumm1 = new CSumm("this->myA->f", Read, List(), 30 )
+    val csumm2 = new CSumm("this->myA", Write, List(), 24 )
+
     if ( true /*csumm1.resource == csumm2.resource*/) {
       val commun_locks = csumm1.lock intersect csumm2.lock
 
@@ -231,6 +256,10 @@ object TraverseJavaClass  {
         update_patches ++ insert_patches
       } else List.empty
 
+      /* The resource in `csumm1` is not synchronized but that in `csumm1` is ==>
+       * INSERT a new synchronization using locks from `csumm2` to protect the
+       * statement manipulating the resource in `csumm1`  */
+
       val patches2 = if (csumm1.lock.length == 0 && csumm2.lock.length > 0) {
         /* ************** INSERTS ***************** */
         /* generate insert objects */
@@ -239,6 +268,10 @@ object TraverseJavaClass  {
         /* generate insert patches */
         generateInsertPatches(filename,inserts,rewriter,tree)
       } else List.empty
+
+      /* The resource in `csumm2` is not synchronized but that in `csumm1` is ==>
+       * INSERT a new synchronization using locks from `csumm` to protect the
+       * statement manipulating the resource in `csumm2`  */
 
       val patches3 = if (csumm1.lock.length > 0 && csumm2.lock.length == 0) {
         /* ************** INSERTS ***************** */
@@ -251,14 +284,32 @@ object TraverseJavaClass  {
 
       val patches = patches1 ++ patches2 ++ patches3
 
-      println("Choose a patch <enter patch id>")
-      val patch_id_str = readLine()
+      if(patches.length >0 ) {
 
-      println("************* GENERATE FIX *************")
-      applyPatch(filename, patch_id_str, patches)
+        println("Choose a patch <enter patch id>")
+        val patch_id_str = readLine()
 
+        println("************* GENERATE FIX *************")
+        applyPatch(filename, patch_id_str, patches)
+      } else {
+
+        /* Both of the resources `csumm1` and `csumm2` are not synchronized ==>
+       * INSERT a new synchronization using a lock on the common resource  */
+
+        val patches4 = if (csumm1.lock.length == 0 && csumm2.lock.length == 0) {
+          /* ************** INSERTS ***************** */
+          /* generate insert objects */
+          val inserts = generateInsertObjectOnCommonResource(csumm1, csumm2)
+
+          /* generate insert patches */
+          generateInsertPatches(filename, inserts, rewriter, tree)
+        } else List.empty
+
+
+        println("************* GENERATE FIX *************")
+        patches4.foreach(elem=> applyPatch_def(filename, elem._1, List(elem)))
+      }
     }
-    /* decide whether to update or insert */
   }
 
   def main(args: Array[String]) = {
